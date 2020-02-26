58c9d95997d283ba1f8e8f87dceab6a927391e45
IoTKETI <iot.keti@gmail.com>
2020-02-26 11:52:57 +0900

    Add files via upload

--- exec.py
+++ exec.py
@@ -7,13 +7,18 @@ import RPi.GPIO as GPIO
 import Adafruit_DHT
 import json
 import paho.mqtt.client as mqtt
+from hx711 import HX711
+from threading import Thread
+import random
+import queue
 
-
+q = queue.Queue()
 
 # Switch
 SW2 = 35
 SW3 = 39
 SW4 = 41
+SW5 = 16 # Debug Switch
 
 # Load Cell
 DAT = 6
@@ -27,6 +32,7 @@ SCL = 31
 DI15 = 11 # temperature & humidity
 
 # Digital OUT
+Buzzer = 17 	# Buzzer
 DO9 = 40 	# LED Green
 DO11 = 38	# LED Yellow
 DO13 = 34	# LED Red
@@ -42,11 +48,28 @@ GPIO.setwarnings(False)
 GPIO.setup(SW2, GPIO.IN, GPIO.PUD_UP)
 GPIO.setup(SW3, GPIO.IN, GPIO.PUD_UP)
 GPIO.setup(SW4, GPIO.IN, GPIO.PUD_UP)
+GPIO.setup(SW5, GPIO.IN, GPIO.PUD_UP)
 
+GPIO.setup(Buzzer, GPIO.OUT)
 GPIO.setup(DO9, GPIO.OUT)
 GPIO.setup(DO11, GPIO.OUT)
 GPIO.setup(DO13, GPIO.OUT)
 GPIO.setup(DO15, GPIO.OUT) 
+
+
+def json_to_val(json_val):
+	payloadData = json.loads(json_val)
+	val = payloadData['val']
+	
+	return (val)
+	
+	
+def val_to_json(val):
+	json_val = {"val":val}
+	json_val = json.dumps(json_val)
+	
+	return (json_val)
+	
 	
 def mqtt_connect(broker_address, port):
 	dry_client = mqtt.Client()
@@ -55,18 +78,37 @@ def mqtt_connect(broker_address, port):
 	dry_client.on_subscribe = on_subscribe
 	dry_client.on_message = on_message
 	dry_client.connect(broker_address, port)
-	dry_client.subscribe("/print_lcd_internal_temp")
-	dry_client.subscribe("/print_lcd_state")
+#	dry_client.subscribe("/print_lcd_internal_temp")
+#	dry_client.subscribe("/print_lcd_state")
+#	dry_client.subscribe("/print_lcd_debug_message")
+	dry_client.subscribe("/print_lcd_loadcell")
+#	dry_client.subscribe("/print_lcd_loadcell_factor")
+#	dry_client.subscribe("/print_lcd_elapsed_time")
+#	dry_client.subscribe("/print_lcd_input_door")
+#	dry_client.subscribe("/print_lcd_output_door")
+#	dry_client.subscribe("/print_lcd_safe_door")
+	dry_client.subscribe("/req_zero_point")
+	dry_client.subscribe("/req_internal_temp")
+	dry_client.subscribe("/req_debug_mode")
+	dry_client.subscribe("/req_start_btn")
+	dry_client.subscribe("/req_calc_factor")
+	dry_client.subscribe("/req_input_door")
+	dry_client.subscribe("/req_output_door")
+	dry_client.subscribe("/req_safe_door")
+	dry_client.subscribe("/req_weight")
+	dry_client.subscribe("/req_operation_mode")
+#	dry_client.subscribe("/set_solenoid")
+#	dry_client.subscribe("/set_fan")
+#	dry_client.subscribe("/set_heater")
+#	dry_client.subscribe("/set_stirrer")
+#	dry_client.subscribe("/set_buzzer")
 
-	for idx in topic:
-		if idx in topic:
-			idx = topic.index(idx)
-			dry_client.subscribe(str(topic[idx]));
-			print('[mqtt_connect] topic[' , idx , ']: ', topic[idx]);
 
 	dry_client.loop_start()
+#	dry_client.loop_forever()
 
 	return dry_client
+
 
 def lcd_init():
 	lcd_columns = 20
@@ -76,8 +118,6 @@ def lcd_init():
 
 	lcd = character_lcd.Character_LCD_I2C(i2c, lcd_columns, lcd_rows)
 	lcd.backlight = True
-
-	print (lcd)
 
 	return lcd
 	
@@ -94,40 +134,25 @@ def on_subscribe(client, userdata, mid, granted_qos):
 	print("subscribed: " + str(mid) + " " + str(granted_qos))
 
 
+def func_set_q(msg):
+	q.put(msg)
+
+
 def on_message(client, userdata, msg):
-	if (msg.topic == '/print_lcd_internal_temp'):
-		print("topic: ", msg.topic)
-		data = msg.payload.decode('utf-8').replace("'", '"')
-		payloadData = json.loads(data)
-		temper = payloadData['val']
-		print ("pre_temp_msg: ", pre_temp_msg, "temper: ", temper)
-		if (pre_temp_msg != temper):
-			pre_temp_msg = temper
-			displayTemp(temper)    
-	elif (msg.topic == '/print_lcd_state'):
-		print("topic: ", msg.topic)
-		data = msg.payload.decode('utf-8').replace("'", '"')
-		payloadData = json.loads(data)
-		state = payloadData['val']
-		print ("pre_state_msg: ", pre_state_msg, "state: ", state)
-		if (pre_state_msg != state):
-			pre_state_msg = state
-			displayState(state)
+	func_set_q(msg)
+	
 
-
-def on_receive_from_msw(topic, str_message):
-	print('[' + topic + '] ' + str_message)
-
-
-def displayTemp(msg):
-		g_lcd.cursor_position(15,0)
-		message = '     '
-		g_lcd.message = message
-		g_lcd.cursor_position(15,0)
-		g_lcd.message = f'{msg}'
+def displayMsg(msg, x, y):
+	g_lcd.cursor_position(x,y)
+	message = '     '
+	g_lcd.message = message
+	g_lcd.cursor_position(x,y)
+	g_lcd.message = msg
 
 
 def displayState(msg):
+	if (msg == 'debug'):
+		g_lcd.clear()
 	g_lcd.cursor_position(0,0)
 	message = '     '
 	g_lcd.message = message
@@ -135,46 +160,72 @@ def displayState(msg):
 	g_lcd.message = msg
 
 
-def init_loadcell(DAT, CLK, tare_weight):
-	EMULATE_HX711=False
+def temp_humi(DHT_PIN):
+	#DHT_SENSOR = Adafruit_DHT.DHT22
+	#humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+	#temperature = round(temperature, 1)
+	
+	#temp = val_to_json(temperature)
+	#print("temperature: ", temperature)
 
+	for i in range(5000):
+		temperature = random.randint(25,30)
+	temp = val_to_json(temperature)
+
+	return (temp)
+	
+	
+def debug_mode():
+	debug_val = GPIO.input(16)
+	debug_val = val_to_json(debug_val)
+
+	return (debug_val)
+	
+
+def ref_weight(tare_weight):
+	tare_weight = val_to_json(tare_weight)
+
+	return (tare_weight)
+
+
+def init_loadcell():
 	# referenceUnit = 1571
 
-	if not EMULATE_HX711:
-		import RPi.GPIO as GPIO
-		from hx711 import HX711
-	else:
-		from emulated_hx711 import HX711
-
-	hx = HX711(DAT, CLK)  # sparrow board pin. hx = HX711(DAT,CLK)
+	hx = HX711(6, 7)  # sparrow board pin. hx = HX711(DAT,CLK)
 	hx.set_reading_format("MSB", "MSB")
-	hx.set_reference_unit(1)
+	hx.set_reference_unit(1517)
 	hx.reset()
 	hx.tare()
+	
+	return hx
+	
 
+def calc_ref_Unit(hx, tare_weight):
+	tare_weight = json_to_val(tare_weight)
 	print("Add weight for initialize...")
-	time.sleep(5)
+	time.sleep(1)
 	total = 0
-	i = 0
-	for i in range(5):
-		print (i+1)
-		weight = max(0, float(hx.get_weight(5)))
+	
+	for i in range(5):	
+		weight = hx.get_weight(5)
 		print(weight)
+		total += weight
 
 	hx.power_down()
 	hx.power_up()
-	total += weight
 			
-	referenceUnit = ((total / (i+1))/tare_weight)
-	print("referenceUnit : ",referenceUnit)
+	referenceUnit = ((total / (i+1)) / tare_weight)
+	referenceUnit = val_to_json(referenceUnit)
+
 	print("Remove weight for initialize...")
-	time.sleep(5)	
+	time.sleep(1)
 	hx.set_reference_unit(referenceUnit)
 	hx.reset()
 	hx.tare()
 	print("Tare done! Add weight now...")
+	referenceUnit = val_to_json(referenceUnit)
 
-	return hx
+	return (hx, referenceUnit)
 
 
 def cleanAndExit():
@@ -189,8 +240,10 @@ def cleanAndExit():
 
 def get_loadcell(hx):
 	try:
-		weight = max(0, float(hx.get_weight(5)))
-		print("Weight : ", round((weight/1000), 1), " kg")
+		weight = hx.get_weight(5)
+		weight = round((weight/1000), 1)
+#		print("Weight : ", weight/1000, " kg")
+		weight = val_to_json(weight)
 
 		hx.power_down()
 		hx.power_up()
@@ -198,27 +251,9 @@ def get_loadcell(hx):
 	except (KeyboardInterrupt, SystemExit):
 		cleanAndExit()
 
-	return (round((weight/1000), 1))
+	return (weight)
 
 	
-def temp_humi(DHT_PIN):
-	DHT_SENSOR = Adafruit_DHT.DHT22
-	humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
-	temperature = round(temperature, 1)
-	humidity = round(humidity, 1)
-
-	if humidity is not None and temperature is not None:
-		temp = {"val":temperature}
-		temp = json.dumps(temp)
-		#dry_client.publish("/get_internal_temp", temp)
-		print("temperature: ", temperature)
-
-	else:
-		print("Failed to retrieve data from humidity sensor")
-
-	return (temp, humidity)
-
-
 def DO_test(DO):
 	print ("LED on" )
 	GPIO.output(DO, GPIO.HIGH) 
@@ -227,35 +262,162 @@ def DO_test(DO):
 	print ("LED off" )
 	GPIO.output(DO, GPIO.LOW)
 	time.sleep(0.5)
-
-######
-#Test#
-######
-
-def main():
-
-	global dry_client
-	broker_address = "203.253.128.161"
-	port = 1883
-	global topic
-	topic = ['/print_lcd_internal_temp']
-
-	global g_lcd
-	g_lcd = lcd_init()
-	dry_client = mqtt_connect(broker_address, port)
-
-
-	global hx
-	#hx = init_loadcell(DAT,CLK, 600)
-	while True:
-		#weight = get_loadcell(hx)
-		temperature, humidity = temp_humi(DI15)
-		dry_client.publish("/get_internal_temp", temperature)
-		#temp1 = temperature
-		#temp2 = humidity
-		#displayMsg(g_lcd)
-		#DO_test(DO15)
-		time.sleep(0.5)
+	
+	
+def switch():
+	SW4 = GPIO.input(41)
+	SW4 = val_to_json(SW4)
 		
-if __name__ == '__main__':
-	main()
+	return (SW4)
+	
+	
+def buzzer(val):
+	GPIO.output(17, val)
+	print ("Beep")	
+		
+
+global dry_client
+broker_address = "localhost"
+port = 1883
+
+global g_lcd
+g_lcd = lcd_init()
+dry_client = mqtt_connect(broker_address, port)
+
+loadcell_ref_weight = 300
+
+global hx
+hx = init_loadcell()
+
+while True:
+	if(q.qsize()):
+		msg = q.get()
+		g_recv_topic = msg.topic;
+		#print(g_recv_topic)
+		if (g_recv_topic == '/req_internal_temp'):
+			#print("topic: ", g_recv_topic)
+			temperature = temp_humi(DI15)
+			dry_client.publish("/res_internal_temp", temperature)
+			
+		elif (g_recv_topic == '/req_debug_mode'):
+			#print("topic: ", g_recv_topic)
+			deb = debug_mode()
+			dry_client.publish("/res_debug_mode", deb)
+			
+		elif (g_recv_topic == '/req_start_btn'):
+			#print("topic: ", g_recv_topic)
+			sw4_json = switch()
+			dry_client.publish("/res_start_btn", sw4_json)	
+
+
+		elif (g_recv_topic == '/req_zero_point'):
+			#print("topic: ", g_recv_topic)
+			reference_weight = ref_weight(loadcell_ref_weight)
+			dry_client.publish("/res_zero_point", reference_weight)
+			
+		elif (g_recv_topic == '/req_calc_factor'):
+			#print("topic: ", g_recv_topic)
+			hx, calc_referenceUnit = calc_ref_Unit(hx, reference_weight)
+			dry_client.publish("/res_calc_factor", calc_referenceUnit)	
+			
+		elif (g_recv_topic == '/req_weight'):
+			#print("topic: ", g_recv_topic)
+			weight = get_loadcell(hx)
+			dry_client.publish("/res_weight", weight)
+			
+		elif (g_recv_topic == '/req_input_door'):
+			#print("topic: ", g_recv_topic)
+			a = 8
+			#dry_client.publish("/res_input_door", weight)
+			
+		elif (g_recv_topic == '/req_output_door'):
+			#print("topic: ", g_recv_topic)
+			a = 7
+			#dry_client.publish("/res_output_door", weight)		
+			
+		elif (g_recv_topic == '/req_safe_door'):
+			#print("topic: ", g_recv_topic)
+			a = 6
+			#dry_client.publish("/res_safe_door", weight)
+					
+		elif (g_recv_topic == '/req_operation_mode'):
+			#print("topic: ", g_recv_topic)
+			a = 1
+			#dry_client.publish("/res_operation_mode", weight)		
+				
+		elif (g_recv_topic == '/print_lcd_internal_temp'):
+			#print("topic: ", g_recv_topic)
+			data = msg.payload.decode('utf-8').replace("'", '"')
+			temper = json_to_val(data)
+			displayMsg(temper, 15,0)    
+			
+		elif (g_recv_topic == '/print_lcd_state'):
+			#print("topic: ", g_recv_topic)
+			data = msg.payload.decode('utf-8').replace("'", '"')
+			state = json_to_val(data)
+			displayState(state)
+			
+		elif (g_recv_topic == '/print_lcd_debug_message'):
+			#print("topic: ", g_recv_topic)
+			data = msg.payload.decode('utf-8').replace("'", '"')
+			debug = json_to_val(data)
+			displayMsg(debug, 0, 3)
+					
+		elif (g_recv_topic == '/print_lcd_loadcell'):
+			#print("topic: ", g_recv_topic)
+			data = msg.payload.decode('utf-8').replace("'", '"')
+			loadcell = json_to_val(data)
+			loadcell = str(loadcell)
+			displayMsg(loadcell,0,1)
+
+		elif (g_recv_topic == '/print_lcd_loadcell_factor'):
+			#print("topic: ", g_recv_topic)
+			data = msg.payload.decode('utf-8').replace("'", '"')
+			loadcell_factor = json_to_val(data)
+			displayMsg(loadcell_factor,15,1)
+			
+		elif (g_recv_topic == '/print_lcd_input_door'):
+			#print("topic: ", g_recv_topic)
+			data = msg.payload.decode('utf-8').replace("'", '"')
+			input_door = json_to_val(data)
+			displayMsg(input_door,0,2)
+			
+		elif (g_recv_topic == '/print_lcd_output_door'):
+			#print("topic: ", g_recv_topic)
+			data = msg.payload.decode('utf-8').replace("'", '"')
+			output_door = json_to_val(data)
+			displayMsg(output_door,6,2)
+			
+		elif (g_recv_topic == '/print_lcd_safe_door'):
+			#print("topic: ", g_recv_topic)
+			data = msg.payload.decode('utf-8').replace("'", '"')
+			safe_door = json_to_val(data)
+			displayMsg(safe_door,13,2)
+			
+		elif (g_recv_topic == '/print_lcd_elapsed_time'):
+			#print("topic: ", g_recv_topic)
+			data = msg.payload.decode('utf-8').replace("'", '"')
+			elapsed_time = json_to_val(data)
+			displayMsg(elapsed_time,6,0)
+						
+		elif (g_recv_topic == '/set_solenoid'):
+			#print("topic: ", g_recv_topic)
+			a=2
+			
+		elif (g_recv_topic == '/set_fan'):
+			#print("topic: ", g_recv_topic)
+			a=3
+			
+		elif (g_recv_topic == '/set_heater'):
+			#print("topic: ", g_recv_topic)
+			a=4 
+			
+		elif (g_recv_topic == '/set_stirrer'):
+			#print("topic: ", g_recv_topic)
+			a=5 
+			
+		elif (g_recv_topic == '/set_buzzer'):
+			#print("topic: ", g_recv_topic)
+			data = msg.payload.decode('utf-8').replace("'", '"')
+			Buzzer = json_to_val(data)
+			buzzer(Buzzer)
